a78e0767658d3f144d82ae407e56cc218bf0e283
Manuel Caruso <mc.manuel.caruso@gmail.com>
2025-01-28 12:47:38 +0100

    feat: added spline animation

--- src/app/pages/welcome-page/welcome-page.page.ts
+++ src/app/pages/welcome-page/welcome-page.page.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Application } from '@splinetool/runtime';
 @Component({
   selector: 'app-welcome-page',
   templateUrl: './welcome-page.page.html',
   styleUrls: ['./welcome-page.page.scss'],
   standalone: false
 })
-export class WelcomePage {
+export class WelcomePage implements OnInit {
 
   constructor() { }
-
+  ngOnInit(){
+    const canvas = document.getElementById('canvas3d') ;
+    const app = new Application(canvas as HTMLCanvasElement);
+    app.load('https://prod.spline.design/d4ndcu3UoIYfqGwo/scene.splinecode'); 
+  }
 }
