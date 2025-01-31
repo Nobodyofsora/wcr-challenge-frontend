950face10986178f560bfd3263618988b3bbc597
Manuel Caruso <mc.manuel.caruso@gmail.com>
2025-01-31 15:34:58 +0100

    fix: moved explore-container to pages

--- src/app/pages/explore-container/explore-container.component.ts
+++ src/app/pages/explore-container/explore-container.component.ts
@@ -1,8 +1,8 @@
 import { ChangeDetectionStrategy, Component, inject, Input, OnInit, signal } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import { PasswordValidator } from '../validators/password.validator';
-import { LoginResponse, RegisterRequest, RegisterResponse } from '../interface/auth.model';
-import { AuthService } from '../services/auth.service';
+import { PasswordValidator } from '../../validators/password.validator';
+import { LoginResponse, RegisterRequest, RegisterResponse } from '../../interface/auth.model';
+import { AuthService } from '../../services/auth.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 
